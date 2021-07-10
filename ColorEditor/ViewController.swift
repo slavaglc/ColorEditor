ce4bf38f20276e38ae8e452c1a6e5c1ddd3e9ccf
slavaglc <47712523+slavaglc@users.noreply.github.com>
2021-07-11 00:01:16 +0300

    Written code

--- ColorEditor/ViewController.swift
+++ ColorEditor/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateColors()
     }
-
-
+    
+    func updateColors() {
+        colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+        redLabel.text = "Red: \(String(format: "%.3f", redSlider.value))"
+        greenLabel.text = "Green: \(String(format: "%.3f", greenSlider.value))"
+        blueLabel.text = "Blue: \(String(format: "%.3f", blueSlider.value))"
+    }
+    
+    @IBAction func sliderChanged() {
+        updateColors()
+    }
 }
-
