c20837dea82db7b8a2ddf81adfa090cedce000f7
slavaglc <47712523+slavaglc@users.noreply.github.com>
2021-07-25 13:35:50 +0300

    fixed constraints

--- ColorEditor/ColorSelectorViewController.swift
+++ ColorEditor/ColorSelectorViewController.swift
@@ -3,24 +3,39 @@ import UIKit
 class ColorSelectorViewController: UIViewController {
 
     @IBOutlet weak var colorView: UIView!
+    
     @IBOutlet weak var redLabel: UILabel!
     @IBOutlet weak var greenLabel: UILabel!
     @IBOutlet weak var blueLabel: UILabel!
+    
     @IBOutlet weak var redSlider: UISlider!
     @IBOutlet weak var greenSlider: UISlider!
     @IBOutlet weak var blueSlider: UISlider!
+    
+    
+    @IBOutlet weak var redTextField: UITextField!
+    @IBOutlet weak var greenTextField: UITextField!
+    @IBOutlet weak var blueTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         updateColor()
     }
     
+    @IBAction func sliderChanged() {
+        updateColor()
+    }
+    
+    
     private func updateColor() {
         colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value),
                                             green: CGFloat(greenSlider.value),
                                             blue: CGFloat(blueSlider.value), alpha: 1)
-        redLabel.text = "Red: \(String(format: "%.3f", redSlider.value))"
-        greenLabel.text = "Green: \(String(format: "%.3f", greenSlider.value))"
-        blueLabel.text = "Blue: \(String(format: "%.3f", blueSlider.value))"
+        redLabel.text = String(format: "%.2f", redSlider.value)
+        redTextField.text = redLabel.text
+        greenLabel.text = String(format: "%.2f", greenSlider.value)
+        greenTextField.text = greenLabel.text
+        blueLabel.text = String(format: "%.2f", blueSlider.value)
+        blueTextField.text = blueLabel.text
     }
 }
---
+++ ColorEditor/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  ColorEditor
+//
+//  Created by slava on 25.07.2021.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
