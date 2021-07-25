12ed5a9c16b2bb2cc8a9dd5edd85dbb2e343c9bf
slavaglc <47712523+slavaglc@users.noreply.github.com>
2021-07-25 16:34:05 +0300

    added setting Primary GUI

--- ColorEditor/ColorSelectorViewController.swift
+++ ColorEditor/ColorSelectorViewController.swift
@@ -1,5 +1,6 @@
 import UIKit
 
+
 class ColorSelectorViewController: UIViewController {
 
     @IBOutlet weak var colorView: UIView!
@@ -17,13 +18,16 @@ class ColorSelectorViewController: UIViewController {
     @IBOutlet weak var greenTextField: UITextField!
     @IBOutlet weak var blueTextField: UITextField!
     
+    var color: UIColor!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        updateColor()
+        setPrimaryGUI()
     }
     
    
     @IBAction func valueChanged(_ sender: UISlider) {
+        print("Starting value changed")
         if sender.tag == 0 {
             redLabel.text = String(format: "%.2f", sender.value)
             redTextField.text = redLabel.text
@@ -34,13 +38,29 @@ class ColorSelectorViewController: UIViewController {
             blueLabel.text = String(format: "%.2f", sender.value)
             blueTextField.text = blueLabel.text
         }
-        updateColor()
+       updateColor()
     }
     
-    
+    private func setPrimaryGUI() {
+        redSlider.value = Float(color.components.red)
+        redTextField.text = String(redSlider.value)
+        redLabel.text = redTextField.text
+        
+        greenSlider.value = Float(color.components.green)
+        greenTextField.text = String(greenSlider.value)
+        greenLabel.text = greenTextField.text
+        
+        blueSlider.value = Float(color.components.blue)
+        blueTextField.text = String(blueSlider.value)
+        blueLabel.text = blueTextField.text
+        updateColor()
+    }
+        
     private func updateColor() {
-        colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value),
-                                            green: CGFloat(greenSlider.value),
-                                            blue: CGFloat(blueSlider.value), alpha: 1)
+        color = UIColor(red: CGFloat(redSlider.value),
+                           green: CGFloat(greenSlider.value),
+                           blue: CGFloat(blueSlider.value), alpha: 1)
+        colorView.backgroundColor = color
+        
     }
 }
--- ColorEditor/MainViewController.swift
+++ ColorEditor/MainViewController.swift
@@ -1,17 +1,25 @@
-//
-//  MainViewController.swift
-//  ColorEditor
-//
-//  Created by slava on 25.07.2021.
-//
-
 import UIKit
 
 class MainViewController: UIViewController {
-
+    var color: UIColor!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        color = view.backgroundColor
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let colorSelectorVC = segue.destination as? ColorSelectorViewController else { return }
+        colorSelectorVC.color = color
+    }
+}
 
+extension UIColor {
+    var coreImageColor: CIColor {
+        return CIColor(color: self)
+    }
+    var components: (red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat) {
+        let coreImageColor = self.coreImageColor
+        return (coreImageColor.red, coreImageColor.green, coreImageColor.blue, coreImageColor.alpha)
+    }
 }
